149dedb1d530558c1aeef7cf06a53918acd96892
crafteryan04 <52469422+crafteryan04@users.noreply.github.com>
2019-07-02 16:07:26 -0400

    Create interact_event.java

---
+++ crafterryan04/me/cratesplugin/interact_event.java
@@ -0,0 +1,17 @@
+package crafterryan04.me.cratesplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class interact_event implements Listener {
+    
+
+}
